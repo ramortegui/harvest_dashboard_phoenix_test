adb84b9baec846b9180b9937eb713708fe2f309c
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-07 19:56:08 -0600

    Add auth and heasers functions.

--- lib/harvest_dashboard/harvest/client_api.ex
+++ lib/harvest_dashboard/harvest/client_api.ex
@@ -1,7 +1,18 @@
 defmodule Harvest.ClientApi do
-  defstruct [:auth]
+  defstruct [:auth, :headers, :connection]
   def new do %Harvest.ClientApi{} end
-  def auth(%Harvest.ClientApi{} = client_api, %Harvest.Organization{} = organization) do
-    %Harvest.ClientApi{ client_api | auth: Base.encode64 "#{organization.username}:#{organization.password}"}
-  end
+    def auth(%Harvest.ClientApi{} = client_api, %Harvest.Organization{} = organization) do
+      %Harvest.ClientApi{ client_api | auth: Base.encode64 "#{organization.username}:#{organization.password}"}
+    end
+
+    def headers( %Harvest.ClientApi{ auth: auth } = client_api ) do
+      headers = [
+        "Accept": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Basic #{auth}",
+        "User-Agent": "API client Ruben"
+      ]
+      %Harvest.ClientApi{ client_api | headers: headers} 
+    end
+
 end

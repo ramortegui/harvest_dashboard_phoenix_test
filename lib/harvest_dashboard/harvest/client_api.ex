ffca661e2a2b6fd57d29f9edc0ef1f45775f0d89
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-09-07 21:37:19 -0600

    Add ClientApi.

--- lib/harvest_dashboard/harvest/client_api.ex
+++ lib/harvest_dashboard/harvest/client_api.ex
@@ -1,21 +1,30 @@
 defmodule Harvest.ClientApi do
-  defstruct [:auth, :headers, :connection]
+  defstruct [:auth, :url, :headers ]
+
   def new do %Harvest.ClientApi{} end
 
-  def auth(%Harvest.ClientApi{} = client_api, %Harvest.Organization{} = organization) do
-    %Harvest.ClientApi{ client_api | auth: Base.encode64 "#{organization.username}:#{organization.password}"}
-  end
+    def headers( %Harvest.ClientApi{} = client_api ) do
+      headers = [
+        "Accept": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
+      ]
+      %Harvest.ClientApi{ client_api | headers: headers}
+    end
 
-  def headers( %Harvest.ClientApi{ auth: auth } = client_api ) do
-    headers = [
-      "Accept": "application/json",
-      "Content-Type": "application/json; charset=utf-8",
-      "Authorization": "Basic #{auth}",
-      "User-Agent": "API client Ruben"
-    ]
-    %Harvest.ClientApi{ client_api | headers: headers}
-  end
 
-  def connection
+    def config( %Harvest.ClientApi{} = client_api, %Harvest.Organization{} = organization, path) do
+      a = %Harvest.ClientApi{ client_api | auth: [hackney: [ basic_auth: { organization.username, organization.password }]], url: "https://#{organization.subdomain}.harvestapp.com/#{path}" }
+      IO.puts inspect a
+      a
+    end
 
+    def request( %Harvest.ClientApi{} = client_api) do
+      HTTPoison.get(client_api.url, client_api.headers, client_api.auth) 
+    end
+
+    def parse_response(response) do
+      case response do
+        {:ok, %HTTPoison.Response{body: body}} -> Poison.decode!(body)
+      end
+    end
 end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule HarvestDashboard.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpotion, "~> 3.0.2"}
+      {:httpoison, "~> 0.13"}
     ]
   end
 
